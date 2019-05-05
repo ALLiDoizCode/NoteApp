c99180e4b2a7547960fbbe23cbe78e2a4f64d8e7
Di3twater <dev.jonathan.green@gmail.com>
2019-05-04 21:06:29 -0400

    Created Form component and added logic for saving note adn clearing form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import Collection from './Components/Collection'
+import Form from './Components/Form'
 import router from './Models/Router'
-import axios from 'axios';
+import axios from 'axios'
 
 var route = router.routes.fetch
 
@@ -25,24 +26,7 @@ const App = () => {
     <div className="container">
       <div className="row"></div>
       <div className="row">
-        <React.Fragment>
-          <div className="row">
-            <form className="col s12">
-              <div className="row">
-                <div className="input-field col s12">
-                  <input id="title" type="text" className="validate"></input>
-                  <label htmlFor="title">Title</label>
-                  <span className="helper-text" data-error="wrong" data-success="right"></span>
-                </div>
-                <div className="input-field col s12">
-                  <textarea id="textarea1" className="materialize-textarea"></textarea>
-                  <label htmlFor="textarea1">Note</label>
-                </div>
-                <button className="btn waves-effect waves-light" type="submit" name="action">Submit<i className="material-icons right">send</i></button>
-              </div>
-            </form>
-          </div>
-        </React.Fragment>
+        <Form/>
       </div>
       <div className="row">
         <div>
---
+++ src/Components/Form.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import router from '../Models/Router'
+import axios from 'axios'
+
+var route = router.routes.save
+
+const Form = () => {
+    console.log("rendering Form")
+    return (
+        <React.Fragment>
+            <div className="row">
+                <form className="col s12" onSubmit={submit}>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="title" type="text" className="validate" onChange = {(e) => note.title = e.target.value}></input>
+                            <label htmlFor="title">Title</label>
+                            <span className="helper-text" data-error="wrong" data-success="right"></span>
+                        </div>
+                        <div className="input-field col s12">
+                            <textarea id="textarea1" className="materialize-textarea" onChange = {(e) => note.text = e.target.value}></textarea>
+                            <label htmlFor="textarea1">Note</label>
+                        </div>
+                        <button className="btn waves-effect waves-light" type="submit" name="action">Submit<i className="material-icons right">send</i></button>
+                    </div>
+                </form>
+            </div>
+        </React.Fragment>
+    );
+}
+
+var note = {
+    _id: "",
+    title: "",
+    text: ""
+}
+const save = () => {
+    const result = axios({ url: router.server + route.endpoint, method: route.method, data:note });
+    result.then((obj) => {
+    })
+}
+
+var submit = (e) => {
+    e.preventDefault();
+    if(!note.text && !note.title) return
+    save()
+    e.target.reset()
+  }
+
+export default React.memo(Form)
