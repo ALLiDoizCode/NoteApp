b765457501202ca5201015bc1aa789c37718708b
Di3twater <dev.jonathan.green@gmail.com>
2019-05-06 16:51:18 -0400

    WIP

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,27 +2,24 @@ import React, { useEffect } from 'react'
 import router from '../Models/Router'
 import axios from 'axios'
 import Collection from './Collection'
-import Modal from './Modal'
 import { useStateValue } from './State'
 
 var route = router.routes.fetch
 
 const Main = () => {
 
-    const [{notes}, dispatch] = useStateValue();
+    const [notes, dispatch] = useStateValue();
 
     const fetch = () => {
         const result = axios({ url: router.server + route.endpoint, method: route.method });
         result.then((obj) => {
-            dispatch({current:{},notes:obj.data})
+            dispatch({current:notes.current,notes:obj.data})
             console.log(notes)
         })
     }
 
     useEffect(() => {
-        setInterval(() => {
-            fetch()
-        }, 2000)
+        fetch()
     }, notes);
 
     return (
@@ -34,4 +31,4 @@ const Main = () => {
     );
 }
 
-export default Main;
+export default React.memo(Main);
